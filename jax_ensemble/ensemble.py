22ca1d98752028dcdd5bbedde2c3169fa64ecfd4
jlehtomaa <jere.lehtomaa@gmail.com>
2022-08-23 21:10:12 +0200

    ensemble cleanup

--- jax_ensemble/ensemble.py
+++ jax_ensemble/ensemble.py
@@ -59,24 +59,6 @@ class Ensemble:
         keys = jrn.split(key, self.num_models)
         self.states = vec_init_fn(keys, mlp_cfg)
 
-    
-
-
-
-    # def _update_targets(self):
-
-    #     if self._target_update_schedule == "hard":
-
-    #         if self._n_updates % self._target_update_freq == 0:
-    #             self.targets = self.targets.replace(
-    #                 params=self.states.params)
-
-    #     elif self._target_update_schedule == "soft":
-    #         self.targets = polyak(self.states, self.targets, self._polyak_tau)
-
-    #     else:
-    #         raise ValueError("Incorrect target update schedule.")
-
     def train(self, batch):
         self.states, losses = bootstrap_train_step_fn(self.states, batch)
         return losses.mean()
@@ -88,29 +70,3 @@ class Ensemble:
     def pred(self, input):
         """Evaluate the ensemble optimistically."""
         return vec_ensemble_pred(self.states, input, self.temperature).squeeze()
-
-    # def pred_single_in(self, x):
-    #     return parallel_pred_single_in_fn(self.targets, x)
-
-    # def pred_multi_in(self, x): # NOTE, THIS GOES TO GENERATE TARGETS
-    #     """
-    #     Evaluate the ensemble model, each member receiving its own input batch.
-    #     """
-    #     return parallel_pred_multi_in_fn(self.targets, x)
-
-    # def batch_eval(self, x): # NOTE, THIS GOES TO THE MPPI ACTION SELECTION
-    #     return parallel_ensemble_pred(self.states, x, self.temp).squeeze()
-
-    # def save_checkpoint(self, chkpt_dir):
-    #     step = self.states.step[0]
-    #     checkpoints.save_checkpoint(chkpt_dir, self.states, step, overwrite=True)
-
-    # def restore_checkpoint(self, chkpt_dir):
-    #     states = checkpoints.restore_checkpoint(chkpt_dir, self.states)
-    #     self.states = self.states.replace(
-    #         params=states.params, priors=states.priors)
-    #     self.targets = deepcopy(self.states)
-    #     #replace(params=new_tar)
-
-
-
